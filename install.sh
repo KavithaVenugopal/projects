1501c29bf2de4a76c262a88a98276a9a36f81087
Ubuntu <ubuntu@ip-172-31-10-58.ap-south-1.compute.internal>
2023-09-23 02:29:21 +0000

    Installation script file

---
+++ install.sh
@@ -0,0 +1,23 @@
+sudo apt update -y
+sudo apt install docker.io -y
+sudo apt install git -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo apt install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo usermod -aG docker ubuntu
+docker-compose --version
+git --version
+docker --version
+jenkins --version
+
